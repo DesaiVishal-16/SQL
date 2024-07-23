ce93f764b14675c5a6e44b0800b8a1374eea6f4d
VishalDesai <desaidesai62230@gmail.com>
2024-07-23 20:54:56 +0530

    Added Contraints

---
+++ Constraints/Constraints.sql
@@ -0,0 +1,45 @@
+-- Constraints
+
+-- Define : Constraints are rules applied to table columns to enforce data integrity and ensure the accuracy and reliability of the data.
+
+1. NOT NULL -- Ensures that a column cannot have a NULL value.
+2. UNIQUE -- Ensures that all values in column are unique.
+3. PRIMARY KEY -- A combination of NOT NULL and UNIQUE. Uniquely identifies each row in a table.
+4. FOREIGN KEY -- Ensures the referential integrity of the data in one table to match values in another table.
+
+
+-- NOT NULL Constraint
+
+CREATE TABLE Users(
+   UserID INT NOT NULL,
+   UserName VARCHAR(50) NOT NULL,
+   UserAge INT NOT NULL
+)
+
+-- UNIQUE Constraint
+
+CREATE TABLE Users(
+   UserID INT NOT NULL,
+   UserName VARCHAR(50) NOT NULL UNIQUE,
+   UserAge INT NOT NULL
+)
+
+-- PRIMARY KEY Constraint
+
+CREATE TABLE Users(
+   UserID INT PRIMARY KEY,
+   UserName VARCHAR(50) NOT NULL UNIQUE,
+   UserAge INT NOT NULL
+   -- PRIMARY Key(UserID)  => Another way to definr Primary key
+)
+
+-- FOREIGN KEY Constraint
+
+CREATE TABLE Orders(
+   OrderID INT,
+   OrderNumber VARCHAR(50) NOT NULL,
+   UserID init,
+   PRIMARY KEY(order),
+   FOREIGN KEY(UserID) REFERENCES User(UserID) )
+)
+
